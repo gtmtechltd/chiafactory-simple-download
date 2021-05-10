a8c5d2e27fc440f71ad16c310fbf2d2db0229f78
gtmtech <geoff@gtmtech.co.uk>
2021-05-10 13:33:36 +0100

    Simple version

---
+++ start.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+DOWNLOAD_DIR=/chiafarms1/dl
+FINAL_DIR=/chiafarms1/dl-complete
+
+if [ -z "${PLOTORDER_API_KEY}" ];then
+  echo "Please set PLOTORDER_API_KEY. Ask support@chiafactory.com for one if you dont have one." >&2
+  exit 1
+fi
+
+set -e
+
+while true; do 
+
+  PLOTS="$( curl -X --silent 'GET' \
+    'https://chiafactory.com/api/v1/plot_orders/' \
+    -H 'accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: Token ${PLOTORDER_API_KEY}" ) | jq -r . )"
+
+  FIRST_PLOT="$( echo "${PLOTS}" | jq -r '[.results[] | select( .status == "R" ) | .plots[] | select( .state == "D" )] | first' )"
+  PLOT_ID="$( echo "${FIRST_PLOT}" | jq -r .id )"
+  DOWNLOAD_URL="$( echo "${FIRST_PLOT}" | jq -r .url )"
+
+  echo "Downloading plot ${PLOT_ID} : ${DOWNLOAD_URL}"
+  (cd "${DOWNLOAD_DIR}" && wget -r --tries=10 "${DOWNLOAD_URL}")
+
+  PLOT_FILE="$( echo "${DOWNLOAD_URL}" | sed -e 's/.*\///g' )"
+
+  echo "Moving plot into ${FINAL_DIR}"
+  mv "${PLOT_FILE}" "${FINAL_DIR}/"
+
+  echo "${PLOT_FILE} downloaded"
+  read -r -p "Downloaded... press enter to continue" VAR
+
+  sleep 600
+
+  echo "Archiving plot id ${PLOT_ID}"
+  curl -X --silent 'PUT' \
+    'https://chiafactory.com/api/v1/plots/${PLOT_ID}' \
+    -H 'accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: Token ${PLOTORDER_API_KEY}" \
+    -d "{
+      \"id\": \"${PLOT_ID}\",
+      \"state\": \"R\"
+    }" \
+  | sed -e 's/^/    /g'
+
+  echo "Deleting plot id ${PLOT_ID}"
+  curl -X --silent 'DELETE' \
+    'https://chiafactory.com/api/v1/plots/${PLOT_ID}' \
+    -H 'accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: Token ${PLOTORDER_API_KEY}" \
+  | sed -e 's/^/    /g'
+
+done
