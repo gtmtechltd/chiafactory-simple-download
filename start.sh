538e2eac987cc22718edf3a0c8be59f1eab35e91
gtmtech <geoff@gtmtech.co.uk>
2021-05-11 06:52:15 +0100

    made logging easier to read

--- start.sh
+++ start.sh
@@ -8,34 +8,40 @@ if [ -z "${PLOTORDER_API_KEY}" ];then
   exit 1
 fi
 
-set -e -x
+log() {
+  echo "$@" >&2
+  echo "$@" >> simple.log
+}
+
+set -e
 
 while true; do 
 
-  echo "Retrieving current plots"
+  log "Retrieving current plots"
   PLOTS="$( curl --silent -X GET \
     'https://chiafactory.com/api/v1/plot_orders/' \
     -H 'accept: application/json' \
     -H 'Content-Type: application/json' \
     -H "Authorization: Token ${PLOTORDER_API_KEY}" )"
 
-  echo "${PLOTS}" | jq -r .
+  echo "${PLOTS}" | jq -r . >> simple.log
   FIRST_PLOT="$( echo "${PLOTS}" | jq -r '[.results[] | select( .status == "R" ) | .plots[] | select( .state == "D" )] | first' )"
   PLOT_ID="$( echo "${FIRST_PLOT}" | jq -r .id )"
   DOWNLOAD_URL="$( echo "${FIRST_PLOT}" | jq -r .url )"
 
+  log "Next plot id ${PLOT_ID}"
   DOWNLOAD_FILE="$( basename "${DOWNLOAD_URL}" )"
   if [ -f "${FINAL_DIR}/${DOWNLOAD_FILE}" ];then
-    echo "Not downloading already-existing plot ${PLOT_ID} : ${DOWNLOAD_URL}"
+    log "* Not downloading already-existing plot ${PLOT_ID} : ${DOWNLOAD_URL}"
   else
-    echo "Downloading plot ${PLOT_ID} : ${DOWNLOAD_URL}"
+    log "- Downloading plot ${PLOT_ID} : ${DOWNLOAD_URL}"
     (cd "${DOWNLOAD_DIR}" && wget -r --tries=10 "${DOWNLOAD_URL}" -O "${DOWNLOAD_FILE}")
-    echo "Moving plot into ${FINAL_DIR}"
+    log "- Moving plot into ${FINAL_DIR}"
     mv "${DOWNLOAD_DIR}/${DOWNLOAD_FILE}" "${FINAL_DIR}/"
-    echo "${DOWNLOAD_FILE} downloaded"
+    log "${DOWNLOAD_FILE} downloaded"
   fi
 
-  echo "Archiving plot id ${PLOT_ID}"
+  log "- Archiving plot id ${PLOT_ID}"
   curl --silent -X PUT \
     "https://chiafactory.com/api/v1/plots/${PLOT_ID}/" \
     -H 'accept: application/json' \
@@ -47,7 +53,7 @@ while true; do
     }" \
   | sed -e 's/^/    /g'
 
-  echo "Deleting plot id ${PLOT_ID}"
+  log "- Deleting plot id ${PLOT_ID}"
   curl --silent -X PUT \
     "https://chiafactory.com/api/v1/plots/${PLOT_ID}/" \
     -H 'accept: application/json' \
